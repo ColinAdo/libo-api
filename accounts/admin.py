090261efe67a14eb5b81fae9166943c26df6a092
ColinAdo <colinadodev30@gmail.com>
2024-02-23 09:26:30 +0300

    Add Custom User Model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "username", 
+        "email", 
+        "display_image",
+        "is_staff", 
+        "is_superuser"
+    ]
+
+    def display_image(self, obj):
+        return mark_safe('<a href="{}"> <img src="{}" width="30" height="30" style="border-radius: 50%;" /> </a>'.format(obj.profile_picture.url, obj.profile_picture.url))
+    display_image.short_description = 'Profile picture'
+
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("profile_picture",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("profile_picture",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    return "profile/{0}/{1}".format(instance.username, filename)
+
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(default='profile.png', upload_to=user_directory_path)
