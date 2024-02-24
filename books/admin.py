d036c1a37cc56e518fdd198300d3114ae38156b8
ColinAdo <colinadodev30@gmail.com>
2024-02-24 09:25:46 +0300

    Add book Category model

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    display_list = ["title"]
+
+
+admin.site.register(Category, CategoryAdmin)
