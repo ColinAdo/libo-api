1db970ff59b312e306bffbaa1114b0c64f40a075
ColinAdo <colinadodev30@gmail.com>
2024-02-24 10:15:57 +0300

    Add Book model

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
-from .models import Category
+from .models import Category, Book
 
 class CategoryAdmin(admin.ModelAdmin):
-    display_list = ["title"]
+    list_display = ["title"]
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "category", 
+        "author", 
+        "title", 
+        "display_image",
+        "read_pdf"
+    ]
+
+    def display_image(self, obj):
+        return mark_safe('<a href="{}"> <img src="{}" width="30" height="30" style="border-radius: 50%;" /> </a>'.format(obj.cover_image.url, obj.cover_image.url))
+    display_image.short_description = 'Cover Image'
+
+    def read_pdf(self, obj):
+        return mark_safe('<a href="{}"> READ IT </a>'.format(obj.pdf_file.url))
+    read_pdf.short_description = 'File'
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,4 +1,9 @@
+from django.conf import settings
 from django.db import models
+
+def book_dir_path(instance, filename):
+    return "book/{0}/{1}".format(instance.title, filename)
+
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
@@ -7,3 +12,17 @@ class Category(models.Model):
         return self.title
 
 
+class Book(models.Model):
+    category = models.ForeignKey(Category, related_name="books", on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    favourites = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="favourite", blank=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes", blank=True)
+    readers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="readers", blank=True)
+    cover_image = models.ImageField(upload_to=book_dir_path)
+    pdf_file = models.FileField(upload_to=book_dir_path)
+    description = models.TextField()
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
