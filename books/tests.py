4f7391fd382a3208f704b25b3f8daa5e8ac548f6
ColinAdo <colinadodev30@gmail.com>
2024-02-24 09:31:04 +0300

    Add category test case

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoryTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(
+            title="Test Category"
+        )
+
+    def test_category_contents(self):
+        self.assertEqual(Category.objects.count(), 1)
+        self.assertEqual(self.category.title, "Test Category")
+
+    def test_category_return_str(self):
+        self.assertEqual(str(self.category), "Test Category")
