2e53776acbdebacf096f0d41556958faef6c9f78
ColinAdo <colinadodev30@gmail.com>
2024-02-24 10:48:39 +0300

    Add  book test case

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Category
+from .models import Category, Book
 
 
 class CategoryTestCase(TestCase):
@@ -17,3 +17,29 @@ class CategoryTestCase(TestCase):
 
     def test_category_return_str(self):
         self.assertEqual(str(self.category), "Test Category")
+
+
+class BookTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.category = Category.objects.create(
+            title="Test Category"
+        )
+
+        cls.book = Book.objects.create(
+            category=cls.category,
+            author="Test Author",
+            title="Test Title"
+        )
+
+    def test_book_contents(self):
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertEqual(self.book.title, "Test Title")
+        self.assertEqual(self.book.author, "Test Author")
+        self.assertEqual(self.book.category.title, "Test Category")
+        self.assertEqual(self.book.is_new(), True)
+
+    def test_book_return_str(self):
+        self.assertEqual(str(self.book), "Test Title")
