b44ec462471c42fa2c0938c284eef47d8336f61c
ColinAdo <colinadodev30@gmail.com>
2024-02-26 08:26:19 +0300

    Add progress test case

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-from .models import Category, Book
+from .models import Category, Book, Progress
 
 
 class CategoryTestCase(TestCase):
@@ -43,3 +44,45 @@ class BookTestCase(TestCase):
 
     def test_book_return_str(self):
         self.assertEqual(str(self.book), "Test Title")
+
+
+class ProgressTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User = get_user_model()
+        cls.user = User.objects.create(
+            username="Test User",
+            email="test@example.com",
+            password="testpassword",
+        )
+        cls.category = Category.objects.create(
+            title="Test Category"
+        )
+
+        cls.book = Book.objects.create(
+            category=cls.category,
+            author="Test Author",
+            title="Test Book Title"
+        )
+
+        cls.progress = Progress.objects.create(
+            user=cls.user,
+            book=cls.book,
+        )
+
+    def test_progress_contents(self):
+        self.assertEqual(Progress.objects.count(), 1)
+        self.assertEqual(self.progress.user, self.user)
+        self.assertEqual(self.progress.book, self.book)
+        self.assertEqual(self.progress.finish_date, None)
+        self.assertEqual(self.progress.is_completed, False)
+        self.assertEqual(str(self.progress), f"{self.user.username} Progress")
+
+    def test_progress_methods(self):
+        self.progress.set_finish_time()
+        self.progress.mark_as_complete()
+
+        self.assertEqual(type(self.progress.remaining_time), int)
+        self.assertEqual(self.progress.is_completed, True)
+        self.assertNotEqual(self.progress.finish_date, None)
