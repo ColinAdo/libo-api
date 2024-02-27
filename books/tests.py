f7349eafacaef026aa65813a5fc4f8d2265023cf
ColinAdo <colinadodev30@gmail.com>
2024-02-27 09:10:48 +0300

    Add Review test case

--- books/tests.py
+++ books/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from .models import Category, Book, Progress
+from .models import Category, Book, Progress, Review
 
 
 class CategoryTestCase(TestCase):
@@ -86,3 +86,37 @@ class ProgressTestCase(TestCase):
         self.assertEqual(type(self.progress.remaining_time), int)
         self.assertEqual(self.progress.is_completed, True)
         self.assertNotEqual(self.progress.finish_date, None)
+
+
+class ReviewTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User = get_user_model()
+        cls.user = User.objects.create(
+            username="Test User",
+            email="test@example.com",
+            password="testpassword",
+        )
+        cls.category = Category.objects.create(
+            title="Test Category"
+        )
+
+        cls.book = Book.objects.create(
+            category=cls.category,
+            author="Test Author",
+            title="Test Book Title"
+        )
+
+        cls.review = Review.objects.create(
+            user=cls.user,
+            book=cls.book,
+            content="Test Book Review"
+        )
+
+    def test_review_contents(self):
+        self.assertEqual(Review.objects.count(), 1)
+        self.assertEqual(self.review.user, self.user)
+        self.assertEqual(self.review.book, self.book)
+        self.assertEqual(self.review.content, "Test Book Review")
+        self.assertEqual(str(self.review), f"Reviewed by {self.user.username}")
