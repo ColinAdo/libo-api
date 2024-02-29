6a53acaded740eb433a6213ca278cf6d3387c569
ColinAdo <colinadodev30@gmail.com>
2024-02-29 08:41:53 +0300

    Add short description

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,19 +1,22 @@
 from django.contrib.auth import get_user_model
 
-from  rest_framework import serializers
+from rest_framework import serializers
 
 from books.models import Book, Review
 
+
 class UserSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = get_user_model()
         fields = ("id", "username")
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
 
 class BookSerializer(serializers.ModelSerializer):
     likes = UserSerializer(many=True, read_only=True)
@@ -22,10 +25,10 @@ class BookSerializer(serializers.ModelSerializer):
     reviews = serializers.SerializerMethodField()
     reviews_count = serializers.SerializerMethodField()
 
-
     readers = UserSerializer(many=True, read_only=True)
     readers_count = serializers.SerializerMethodField()
 
+    description = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
@@ -50,17 +53,19 @@ class BookSerializer(serializers.ModelSerializer):
     def get_likes_count(self, obj):
         count = len(obj.likes.all())
         return count
-    
+
     def get_reviews(self, obj):
         qs = obj.reviews.all()
         serializer = ReviewSerializer(qs, many=True).data
         return serializer
-    
+
     def get_reviews_count(self, obj):
         count = len(obj.reviews.all())
         return count
-    
+
     def get_readers_count(self, obj):
         count = len(obj.readers.all())
         return count
 
+    def get_description(self, obj):
+        return f"{obj.description[:80]}..."
