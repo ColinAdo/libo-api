6cd7b129e33c29a66d91b8c55c1cfaebf3ab648d
ColinAdo <colinadodev30@gmail.com>
2024-04-22 14:55:02 +0300

    Add username and email field to custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,12 @@ def user_directory_path(instance, filename):
     return "profile/{0}/{1}".format(instance.username, filename)
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255)
     profile_picture = models.ImageField(default='profile.png', upload_to=user_directory_path)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
