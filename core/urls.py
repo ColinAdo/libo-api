6b60e2ca42cbb15821e23be0cb2a6fd189573df8
ColinAdo <colinadodev30@gmail.com>
2024-04-22 14:54:16 +0300

    Add djoser URLConf

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.jwt')),
     path('api/v1/', include('books.api.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
