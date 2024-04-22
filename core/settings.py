d368a5e9251c1c10ed215bb2c1b86ff095083dd9
ColinAdo <colinadodev30@gmail.com>
2024-04-22 12:04:09 +0300

    Add environment variables settings

--- core/settings.py
+++ core/settings.py
@@ -1,20 +1,30 @@
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+import dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = BASE_DIR / '.env.local'
+
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', 'False') == 'True'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*70-w$=ro^vg^_ad_aadyqy^#g@%gxt2eiz(fc$@$4#i6kvu9@'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False) == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1:8000,localhost:8000').split(',')
 
 
 # Application definition
