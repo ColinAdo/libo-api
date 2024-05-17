e1285c5dc7ed97a60ceeb32c37e19ec49872cfd3
ColinAdo <colinadodev30@gmail.com>
2024-05-17 22:52:18 +0300

    Removed progresses related data

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
 
-from .models import Category, Book, Progress, Review
+from .models import Category, Book, Review
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["title"]
@@ -23,14 +23,6 @@ class BookAdmin(admin.ModelAdmin):
         return mark_safe('<a href="{}"> READ IT </a>'.format(obj.pdf_file.url))
     read_pdf.short_description = 'File'
 
-class ProgressAdmin(admin.ModelAdmin):
-    list_display = [
-        "book",
-        "user",
-        "start_date",
-        "finish_date",
-        "is_completed",
-    ]
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = [
@@ -41,5 +33,4 @@ class ReviewAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Book, BookAdmin)
-admin.site.register(Progress, ProgressAdmin)
 admin.site.register(Review, ReviewAdmin)
--- books/models.py
+++ books/models.py
@@ -32,34 +32,6 @@ class Book(models.Model):
         return self.title
 
 
-class Progress(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    start_date = models.DateTimeField(auto_now_add=True)
-    finish_date = models.DateTimeField(blank=True, null=True)
-    is_completed = models.BooleanField(default=False)
-
-    def set_finish_time(self):
-        # Change the finish date
-        self.finish_date = self.start_date + timedelta(minutes=2) 
-        self.save()
-
-    @property
-    def remaining_time(self):
-        if self.finish_date:
-            time_remaining = self.finish_date - timezone.now()
-            # getting total seconds
-            return int(time_remaining.total_seconds() / 60)
-        return None
-
-    def mark_as_complete(self):
-        self.is_completed = True
-        self.finish_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return f"{self.user.username} Progress"
-
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     book = models.ForeignKey(Book, related_name="reviews" ,on_delete=models.DO_NOTHING)
