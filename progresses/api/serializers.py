8f72e50288e9537d1910606f89b82eba567c5a3c
ColinAdo <colinadodev30@gmail.com>
2024-05-17 23:27:12 +0300

    Add user fields as readonly fields

--- progresses/api/serializers.py
+++ progresses/api/serializers.py
@@ -3,11 +3,13 @@ from rest_framework import serializers
 from progresses.models import Progress
 
 class ProgressSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Progress
         fields = (
             'id',
             'book',
+            'user',
             'start_date',
             'finish_date',
             'is_completed',
