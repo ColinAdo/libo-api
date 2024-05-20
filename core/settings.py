aacf08e8c879f9f81f57da9a460059a54b15095e
ColinAdo <colinadodev30@gmail.com>
2024-05-20 21:47:39 +0300

    Add favourites app

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'books.apps.BooksConfig',
     'progresses.apps.ProgressesConfig',
+    'favourites.apps.FavouritesConfig',
 ]
 
 MIDDLEWARE = [
