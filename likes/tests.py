28cc6ce5d940b9f785bc515428c3288d94d13ad5
ColinAdo <colinadodev30@gmail.com>
2024-05-21 22:46:09 +0300

    Add test like reviews

--- likes/tests.py
+++ likes/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import CustomUser
+from reviews.models import Review
+from likes.models import LikeReview
+from books.models import Book, Category
+
+
+class TestLikeReview(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpassword'
+        )
+        cls.category = Category.objects.create(
+            title="Test Category"
+        )
+
+        cls.book = Book.objects.create(
+            category=cls.category,
+            author="Test Author",
+            title="Test Title"
+        )
+
+        cls.review = Review.objects.create(
+            user=cls.user,
+            book=cls.book,
+            content='first review'
+        )
+
+        cls.like = LikeReview.objects.create(
+            user=cls.user,
+            review=cls.review
+        )
+
+    def test_like_post(self):
+        self.assertEqual(LikeReview.objects.count(), 1)
+        self.assertEqual(self.like.user, self.user)
+        self.assertEqual(self.like.review, self.review)
+        self.assertEqual(str(self.like), f'{self.user.username} likes')
