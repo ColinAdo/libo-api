e6f9005f3c8cb7ba781b75a56733e2ba5d019ad9
ColinAdo <colinadodev30@gmail.com>
2024-05-21 22:36:54 +0300

    Add likes to reviews serializers

--- reviews/api/serializers.py
+++ reviews/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from reviews.models import Review
+from likes.api.serializers import LikeReviewSeralizer
 
 class ReviewSerializer(serializers.ModelSerializer):
     reviewed_by = serializers.ReadOnlyField(source='user.username')
+
+    likes = LikeReviewSeralizer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+
     class Meta:
         model = Review
         fields = (
@@ -11,5 +20,7 @@ class ReviewSerializer(serializers.ModelSerializer):
             'reviewed_by',
             'book',
             'content',
+            'likes',
+            'likes_count',
             'date'
         )
