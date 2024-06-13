786ebceeb87436c9b3685bd0f66a9fa8d7993ee7
ColinAdo <colinadodev30@gmail.com>
2024-06-13 13:32:33 +0300

    Add Read book URLConf

--- progresses/api/urls.py
+++ progresses/api/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProgressViewset
+from .views import ProgressViewset, ReadView
 
 route = DefaultRouter()
 route.register(r'progress', ProgressViewset, basename='progress')
-urlpatterns = route.urls
+
+urlpatterns = [
+    path('read/<int:pk>/', ReadView.as_view(), name='read'),
+]
+urlpatterns += route.urls
