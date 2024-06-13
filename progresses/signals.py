ce84c3ab1b66ca828268213001efddf1f90158d8
ColinAdo <colinadodev30@gmail.com>
2024-06-13 13:30:36 +0300

    Add signals file to send email after 7 days

---
+++ progresses/signals.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from .models import Progress
+
+
+from celery import shared_task
+
+@shared_task
+def send_completion_email_after_seven_days(progress_id):
+    progress = Progress.objects.get(id=progress_id)
+    if progress.remaining_time == 0:
+        progress.mark_as_complete()
+        progress.save()
+
+        domain = f'http://localhost:8000/{progress.book.pdf_file.url}'
+        subject = f'{progress.book.title} completion'
+        message = f'Hey {progress.user.username}, \n\n \
+                You have completed reading {progress.book.title} book. \n\n \
+                You can download it from here {domain}. \n\n \
+                Never stop learning, remember readers are leaders! \n\n \
+                The Libo http://localhost:8000 team.'
+
+        send_mail(
+            subject,  
+            message, 
+            settings.AWS_SES_FROM_EMAIL, 
+            [f'{progress.user.email}'], 
+        )
+       
+        
