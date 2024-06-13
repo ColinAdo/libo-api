6a6dccbef824255e87dbead52030bae26961cdf0
ColinAdo <colinadodev30@gmail.com>
2024-06-13 13:31:57 +0300

    Add imported signals file to apps

--- progresses/apps.py
+++ progresses/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProgressesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'progresses'
+
+    def ready(self):
+        import progresses.signals
