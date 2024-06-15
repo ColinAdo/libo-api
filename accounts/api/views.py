5e563b69b01d31f9cee52e04dcaa5fb2f67b4b72
ColinAdo <colinadodev30@gmail.com>
2024-06-15 22:14:33 +0300

    Add user profile data

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import permissions, status
+from rest_framework import permissions
 
 from progresses.models import Progress
 from progresses.api.serializers import ProgressSerializer
 from favourites.models import Favourite
 from favourites.api.serializers import FavouriteSerializer
+from .serializers import UserSerializer
 
 class ProfileView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -23,14 +24,18 @@ class ProfileView(APIView):
             reading_books_data = ProgressSerializer(reading_books, many=True).data
             read_books_data = ProgressSerializer(read_books, many=True).data
             book_saved_data = FavouriteSerializer(book_saved, many=True).data
+            profile = UserSerializer(user).data
 
             if reading_books_data or read_books_data or book_saved_data:
                 response_data = {
+                    'profile': profile,
                     'reading_books': reading_books_data,
                     'read_books': read_books_data,
                     'book_saved': book_saved_data
                 }
                 return Response(response_data)
             else:
-                return Response({"Message": "Your library is empty..."})
+                return Response(profile)
+        
+        return Response({"Message": "You are not the owner..."})
 
