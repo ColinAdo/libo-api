fd22e30e0781421ed4872223f880dfa8e1a31a34
ColinAdo <colinadodev30@gmail.com>
2024-06-15 22:27:52 +0300

    Add proper add book to favourite

--- favourites/api/views.py
+++ favourites/api/views.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth import get_user_model
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +7,7 @@ from rest_framework import permissions
 from .serializers import FavouriteSerializer
 from favourites.models import Favourite
 from books.models import Book
+
 
 class FavouriteView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -18,9 +18,9 @@ class FavouriteView(APIView):
             favourite = Favourite.objects.filter(user=request.user)
             serializer = FavouriteSerializer(favourite, many=True)
 
-            return Response({"success": True, "favorite_books": serializer.data})
+            return Response({"favorite_books": serializer.data})
         except ObjectDoesNotExist:
-            return Response({"success": True, "error": "favourite book does not exists" })
+            return Response({"error": "favourite book does not exists" })
         
     def post(self, request, pk):
         try:
@@ -29,10 +29,10 @@ class FavouriteView(APIView):
 
             if not new_favourite_book[1]:
                 new_favourite_book[0].delete()
-                return Response({"success": True, "message": "Book removed from favourites list"})
+                return Response({"message": "Book removed from favourites list"})
             else:
-                return Response({"success": True, "message": "Book added to favourites list"})
+                return Response({"message": "Book added to favourites list"})
 
         except ObjectDoesNotExist:
-            return Response({"success": True, "error": "book does not exists"})
+            return Response({"error": "book does not exists"})
 
