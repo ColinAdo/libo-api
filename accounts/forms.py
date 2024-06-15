7262a148461642c242e1e018741eb1ec04e152b8
ColinAdo <colinadodev30@gmail.com>
2024-06-15 22:31:37 +0300

    Add comment to forms file

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from accounts.models import CustomUser
 
+#  Custom user creation from 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("profile_picture",)
 
-
+#  Custom user change/update from 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
