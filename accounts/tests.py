123f888622c59777baf80b591ba04e780ade6f33
ColinAdo <colinadodev30@gmail.com>
2024-06-28 21:05:39 +0300

    Add CustomUser model test case

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import CustomUser
 
-# Create your tests here.
+class CustomUserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create(
+            username='testuser',
+            email='testuser@example.com',
+        )
+
+    def test_user_model_content(self):
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.email, 'testuser@example.com')
+        self.assertEqual(str(self.user), 'testuser')
