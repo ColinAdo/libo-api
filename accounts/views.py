2f4713f2a0eb435cc74a7ac77dcf2fa6669dbd35
ColinAdo <colinadodev30@gmail.com>
2024-07-08 16:44:25 +0300

    Add comment to view

--- accounts/views.py
+++ accounts/views.py
@@ -94,7 +94,7 @@ class CustomTokenRefreshView(TokenRefreshView):
 
         return response
 
-
+# Custom refresh token
 class CustomTokenVerifyView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
         access_token = request.COOKIES.get('access')
