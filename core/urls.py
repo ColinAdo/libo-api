dc8aae696d388ba120c239367be203b56113fcec
ColinAdo <colinadodev30@gmail.com>
2024-07-11 21:16:33 +0300

    Add spectacular URLConf

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView, 
+    SpectacularRedocView, 
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +18,8 @@ urlpatterns = [
     path('api/v1/', include('favourites.api.urls')),
     path('api/v1/', include('reviews.api.urls')),
     path('api/v1/', include('likes.api.urls')),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
