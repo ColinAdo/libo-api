30c32e205385654881b41dd6dbd4bb4364ead553
ColinAdo <colinadodev30@gmail.com>
2024-07-11 21:28:22 +0300

    Add comment to viewset

--- books/api/views.py
+++ books/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from books.models import Book
 from books.api.serializers import BookSerializer
 
-
+# Book viewset
 class BookViewset(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
