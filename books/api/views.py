9c44d734d562e82e8243d7f09a3992a394a2326a
ColinAdo <colinadodev30@gmail.com>
2024-07-11 21:25:47 +0300

    Add clean code

--- books/api/views.py
+++ books/api/views.py
@@ -1,14 +1,8 @@
-from django.conf import settings
-
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from books.models import Book
 from books.api.serializers import BookSerializer
 
-import openai
-from openai.error import RateLimitError
 
 class BookViewset(viewsets.ModelViewSet):
     queryset = Book.objects.all()
