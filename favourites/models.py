fbf7acca7350026f84a93ffc8579e8e2a1016ff0
ColinAdo <colinadodev30@gmail.com>
2024-07-22 20:06:18 +0300

    Add comment to favorite model

--- favourites/models.py
+++ favourites/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 from books.models import Book
 
+# Favorite model
 class Favourite(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
