8df0d2d3b8374614b11a360a7418c5a51806b365
ColinAdo <colinadodev30@gmail.com>
2024-07-24 08:16:51 +0300

    Add comment to likes serializer

--- likes/api/serializers.py
+++ likes/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from likes.models import LikeReview
 
+# Like review serializer
 class LikeReviewSeralizer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     review = serializers.PrimaryKeyRelatedField(read_only=True)
