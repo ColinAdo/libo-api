cdf54edc3ac8c2991582d590049c73303f1b25b9
ColinAdo <colinadodev30@gmail.com>
2024-08-07 19:45:16 +0300

    Add single quotes

--- reviews/tests.py
+++ reviews/tests.py
@@ -11,29 +11,29 @@ class ReviewTestCase(TestCase):
     def setUpTestData(cls):
         User = get_user_model()
         cls.user = User.objects.create(
-            username="Test User",
-            email="test@example.com",
-            password="testpassword",
+            username='Test User',
+            email='test@example.com',
+            password='testpassword',
         )
         cls.category = Category.objects.create(
-            title="Test Category"
+            title='Test Category'
         )
 
         cls.book = Book.objects.create(
             category=cls.category,
-            author="Test Author",
-            title="Test Book Title"
+            author='Test Author',
+            title='Test Book Title'
         )
 
         cls.review = Review.objects.create(
             user=cls.user,
             book=cls.book,
-            content="Test Book Review"
+            content='Test Book Review'
         )
 
     def test_review_contents(self):
         self.assertEqual(Review.objects.count(), 1)
         self.assertEqual(self.review.user, self.user)
         self.assertEqual(self.review.book, self.book)
-        self.assertEqual(self.review.content, "Test Book Review")
-        self.assertEqual(str(self.review), f"Reviewed by {self.user.username}")
+        self.assertEqual(self.review.content, 'Test Book Review')
+        self.assertEqual(str(self.review), f'Reviewed by {self.user.username}')
