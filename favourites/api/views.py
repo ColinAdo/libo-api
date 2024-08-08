9856242730d16e86d71285ce9ecdfee4cecb9e54
ColinAdo <colinadodev30@gmail.com>
2024-08-08 11:48:06 +0300

    Add single quotes

--- favourites/api/views.py
+++ favourites/api/views.py
@@ -18,9 +18,9 @@ class FavouriteView(APIView):
             favourite = Favourite.objects.filter(user=request.user)
             serializer = FavouriteSerializer(favourite, many=True)
 
-            return Response({"favorite_books": serializer.data})
+            return Response({'favorite_books': serializer.data})
         except ObjectDoesNotExist:
-            return Response({"error": "favourite book does not exists" })
+            return Response({'error': 'favourite book does not exists' })
         
     def post(self, request, pk):
         try:
@@ -29,10 +29,10 @@ class FavouriteView(APIView):
 
             if not new_favourite_book[1]:
                 new_favourite_book[0].delete()
-                return Response({"message": "Book removed from favourites list"})
+                return Response({'message': 'Book removed from favourites list'})
             else:
-                return Response({"message": "Book added to favourites list"})
+                return Response({'message': 'Book added to favourites list'})
 
         except ObjectDoesNotExist:
-            return Response({"error": "book does not exists"})
+            return Response({'error': 'book does not exists'})
 
