9627f4456cf786f32e82362ab75a78db2412dfb7
ColinAdo <colinadodev30@gmail.com>
2024-08-26 19:48:26 +0300

    Add proper code structure

--- reviews/models.py
+++ reviews/models.py
@@ -11,4 +11,4 @@ class Review(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Reviewed by {self.user.username}"
+        return f'Reviewed by {self.user.username}'
