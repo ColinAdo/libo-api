503c7320fba43b89adf6606537505026b8e7b6cd
ColinAdo <colinadodev30@gmail.com>
2025-02-04 10:14:15 +0300

    Add proper code

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import ProfileView
+# from .views import ProfileView
 
 urlpatterns = [
-    path('profile/<str:profId>/', ProfileView.as_view(), name='profile'),
+    # path('profile/<str:profId>/', ProfileView.as_view(), name='profile'),
 ]
 
