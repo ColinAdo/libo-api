619138d7aaa3c41204b66ad238d224fe72e2f728
ColinAdo <colinadodev30@gmail.com>
2025-02-04 09:41:05 +0300

    Delete review model

--- reviews/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import ReviewsViewset
-
-router = DefaultRouter()
-
-router.register(r'reviews', ReviewsViewset, basename='reviews')
-urlpatterns = router.urls
