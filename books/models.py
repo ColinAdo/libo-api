6d80ba5a5281a180e0de057208f107da8785eae3
ColinAdo <colinadodev30@gmail.com>
2025-02-06 08:46:44 +0300

    Removed some comments

--- books/models.py
+++ books/models.py
@@ -4,10 +4,6 @@ from django.utils import timezone
 
 from datetime import timedelta
 
-# Book directory path
-# def book_dir_path(instance, filename):
-#     return 'book/{0}/{1}'.format(instance.author, filename)
-
 # category model
 class Category(models.Model):
     title = models.CharField(max_length=200)
