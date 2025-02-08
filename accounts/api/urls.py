95f669c7b8d0116be7c9e96bfaf136aa263d3c7f
ColinAdo <colinadodev30@gmail.com>
2025-02-08 09:59:47 +0300

    Deleted unwanted files

--- accounts/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-# from .views import ProfileView
-
-urlpatterns = [
-    # path('profile/<str:profId>/', ProfileView.as_view(), name='profile'),
-]
-
--- core/urls.py
+++ core/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('accounts.urls')),
-    path('api/v1/', include('accounts.api.urls')),
     path('api/v1/', include('books.api.urls')),
     path('api/v1/', include('progresses.api.urls')),
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
