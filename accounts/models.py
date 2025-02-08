da08f244c9e2b5f2d7f1b4622b1d20192734ba9b
ColinAdo <colinadodev30@gmail.com>
2025-02-08 10:00:46 +0300

    Removed profile picture from model

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from shortuuid.django_fields import ShortUUIDField
-
-# user directory path
-def user_directory_path(instance, filename):
-    return 'profile/{0}/{1}'.format(instance.username, filename)
-
-characters = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDFGHIJKLMNOPQRSTUVWXYZ'
 
 #  Custom user model
 class CustomUser(AbstractUser):
-    profId = ShortUUIDField(unique=True, length=10, max_length=20, alphabet=characters)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=255)
-    profile_picture = models.ImageField(default='profile.png', upload_to=user_directory_path)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
