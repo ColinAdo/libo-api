15b0713345cbe531a287f3aeefca5e97c66a4b89
ColinAdo <colinadodev30@gmail.com>
2025-05-10 12:08:03 +0300

    Add Liked books url

--- books/api/urls.py
+++ books/api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 from books.api.views import (
     BookViewset, 
     CategoryView, 
+    LikedBooksView,
     BookCategoryView
 )
 
@@ -12,6 +13,7 @@ routes = DefaultRouter()
 
 routes.register(r'books', BookViewset, basename='books')
 urlpatterns = [
+    path('liked/books/', LikedBooksView.as_view(), name='likes'),
     path('categories/', CategoryView.as_view(), name='categories'),
     path('books/category/<int:id>/', BookCategoryView.as_view(), name='book-category'),
 ]
